2dfe2a1ddea074027b6587b91c7dc2a25ddfa223
Dmitry Nikolayev <reacttsxgrammer@gmail.com>
2023-11-25 11:58:47 -0600

    Feat: create Header/Body/Footer

--- src/App.js
+++ src/App.js
@@ -1,23 +1,28 @@
 import logo from './logo.svg';
 import './App.css';
+import './styles/globals.scss';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+        <div className="Header">
+            <div className="Logo">
+            <img src={logo} className="App-logo" alt="logo" />
+            </div>
+            <div className="Title">
+            <h1>React App</h1>
+            </div>
+        </div>
+        <div className="Body">
+            <div className="Button">
+            <button>Click Me</button>
+            </div>
+        </div>
+        <div className="Footer">
+            <div className="Text">
+            <p>Footer</p>
+            </div>
+        </div>
     </div>
   );
 }
